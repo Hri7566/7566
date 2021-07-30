30e852d2d5e7f02b5e73a5e89736c3680b65ff81
Hri7566 <hri7566@gmail.com>
2021-07-30 10:32:31 -0400

    js command fix

--- src/commands/js.js
+++ src/commands/js.js
@@ -3,7 +3,7 @@ const Command = require("../Command");
 module.exports = new Command('js', ['js'], `%PREFIX%js (eval string)`, `Evaluate JavaScript during runtime.`, async (msg, cl) => {
     let out = `✔️ Console: `;
     try {
-        if (msg.argcat.toLowerCase().includes('process')) return `Process is disallowed.`;
+        // if (msg.argcat.toLowerCase().includes('process')) return `Process is disallowed.`;
         let ev = eval(msg.argcat);
         out += `(${typeof(ev)}) ` + eval(msg.argcat);
     } catch (err) {
