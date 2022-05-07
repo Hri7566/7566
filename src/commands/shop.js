b55ce229a0c2cad4b47f46854f713cd923546436
Hri7566 <hri7566@gmail.com>
2022-05-06 22:17:02 -0400

    hide shop

--- src/commands/shop.js
+++ src/commands/shop.js
@@ -2,4 +2,4 @@ const Command = require("../Command");
 
 module.exports = new Command('shop', ['shop'], `%PREFIX%shop`, undefined, (msg, cl) => {
     return 'unfinished :(';
-}, 0, 0, false);
+}, 0, 0, true);
