f96bdd3482437a2f2f3f981251c63b102ee61977
Hri7566 <hri7566@gmail.com>
2022-05-15 10:14:48 -0400

    fix deprecation

--- src/commands/users.js
+++ src/commands/users.js
@@ -2,6 +2,6 @@ const Command = require("../Command");
 const { Database, User } = require("../Database");
 
 module.exports = new Command('users', ['users', 'usercount'], `%PREFIX%users`, undefined, async (msg, cl) => {
-    let str = "Users in database: " + (await User.count());
+    let str = "Users in database: " + (await User.countDocuments());
     return str;
 }, 0, 0, false);
