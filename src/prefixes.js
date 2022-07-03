515890da83efbdb9ed106cc1d6bf485f070c5cd0
Hri7566 <hri7566@gmail.com>
2022-07-03 14:16:03 -0400

    change prefix (urgent)

--- src/prefixes.js
+++ src/prefixes.js
@@ -4,13 +4,13 @@ let prefixes = new Map();
 
 if (process.env.NODE_ENV === "production") {
     // prefixes.set('asterisk', new Prefix('asterisk', '*'));
-    prefixes.set('dot', new Prefix('dot', '.'));
+    prefixes.set('dot', new Prefix('asterisk', '*'));
 } else {
     prefixes.set('asterisk', new Prefix('asterisk', '**'));
     // prefixes.set('caret', new Prefix('caret', '^'));
     // prefixes.set('slash', new Prefix('slash', '/'));
 }
-prefixes.set('test', new Prefix('test', 'newprefix!'));
+// prefixes.set('test', new Prefix('test', 'newprefix!'));
 prefixes.set('7', new Prefix('7', '7'));
 // prefixes.set('caret', new Prefix('caret', '^'));
 
