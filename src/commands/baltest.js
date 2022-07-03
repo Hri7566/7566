834d1f6443d9cb44251ce1b9f6da8c124f84120b
Hri7566 <hri7566@gmail.com>
2022-07-03 13:39:33 -0400

    comment out all log statements

--- src/commands/baltest.js
+++ src/commands/baltest.js
@@ -5,7 +5,7 @@ module.exports = new Command('baltest', ['baltest'], undefined, undefined, (msg,
     return 'no';
     if (!parseInt(msg.argcat)) return 'no';
     let bal = new Balance(parseInt(msg.argcat));
-    console.log(bal);
+    // console.log(bal);
     return bal + "";
     // return 'no more testing for u';
 }, 0, 0, true);
